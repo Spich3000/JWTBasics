56075599d7b495e85b01de394e87e24641812645
Spich <spichakovda@gmail.com>
2023-09-03 05:05:44 +0300

    requireq userName and password in request

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,24 @@
+/*
+Check userName, password in post(login) request
+if exist -> create new JWT
+send back to front-end
 
+setup authentification so only request with JWT can access the dashboard
+*/
+
+const CustomAPIError = require("../errors/custom-error")
 
 const login = async (req, res) => {
+    const { userName, password } = req.body
+
+    // mongoose validation
+    // Joi
+    // Check in the controller
+
+    if (!userName || !password) {
+        throw new CustomAPIError("Please provide email and password", 400)
+    }
+
     res.send("Fake Login/Register/Signup Route")
 }
 
